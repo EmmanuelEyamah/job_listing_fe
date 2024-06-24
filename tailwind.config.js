eea14999ca6e290a23eb970b1020a05014f76b20
Manny <officialeminz@gmail.com>
2024-06-24 12:18:22 -0700

    ui: plugins

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+import withMT from "@material-tailwind/react/utils/withMT";
+
+const config = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default withMT(config);
